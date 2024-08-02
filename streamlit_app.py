d3ff7ddd5b3ab8f5690f825af7cbcd79f8324417
Wayne Saunders <wayne@pdc.is>
2024-08-02 09:09:44 +0000

    add query param

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,6 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+if st.query_params.user:
+    st.write(st.query_params['user'])
